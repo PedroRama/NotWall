f831b51d370fc9c1ddefa318906ed7d8626d6b45
Pedro <pramag22@fpcoruna.afundacion.org>
2024-05-14 01:00:57 +0200

    Pantallas creadas y prueba de botone calculadora

---
+++ Front/app/src/main/java/com/example/notwall/calc_MEdiaCompra.java
@@ -0,0 +1,14 @@
+package com.example.notwall;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class calc_MEdiaCompra extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calc_media_compra);
+    }
+}
