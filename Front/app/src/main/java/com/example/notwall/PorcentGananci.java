2a859cbccd612b39906a7f841c71dbc90ff35800
Pedro <pramag22@fpcoruna.afundacion.org>
2024-05-15 19:27:14 +0200

    Todo funcionando

---
+++ Front/app/src/main/java/com/example/notwall/PorcentGananci.java
@@ -0,0 +1,14 @@
+package com.example.notwall;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PorcentGananci extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_porcent_gananci);
+    }
+}
